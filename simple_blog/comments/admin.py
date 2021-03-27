5d1a4a5a1f0768067d6ee875be18a68b67364863
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-03-27 14:16:15 +0700

    add comments model

---
+++ simple_blog/comments/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Comment
+# admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['comment','created','created_by','blog','is_spam']
+    list_filter= ['created']
